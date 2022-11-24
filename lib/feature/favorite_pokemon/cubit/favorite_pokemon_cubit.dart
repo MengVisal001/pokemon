6dd5843dd70e5ca54c01f899e2b538d5c8f1900d
Meng Visal <mengvisal.in@gmail.com>
2022-11-24 19:02:04 +0700

    update

--- lib/feature/favorite_pokemon/cubit/favorite_pokemon_cubit.dart
+++ lib/feature/favorite_pokemon/cubit/favorite_pokemon_cubit.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pokemon/model/pokemon_model.dart';
-import 'package:pokemon/widget/global_constant.dart';
+import 'package:pokemon/resources/global_constant.dart';
 
 part 'favorite_pokemon_state.dart';
 
