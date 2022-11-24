01f7ce2d99fa59ece416bb18b818090d89cf4d07
Meng Visal <mengvisal.in@gmail.com>
2022-11-24 18:48:08 +0700

    update

--- lib/feature/favorite_pokemon/cubit/favorite_pokemon_cubit.dart
+++ lib/feature/favorite_pokemon/cubit/favorite_pokemon_cubit.dart
@@ -8,6 +8,11 @@ class FavoritePokemonCubit extends Cubit<AddFavoritePokemon> {
   FavoritePokemonCubit() : super(AddFavoritePokemon(pokemonList: []));
 
   void addToFavorite(PokemonModel pokemonModel) async {
+    if (tempPokemon.isNotEmpty) {
+      for (var element in tempPokemon) {
+        if (pokemonModel.id == element.id) return;
+      }
+    }
     tempPokemon.add(pokemonModel);
     emit(AddFavoritePokemon(isFavorite: true, pokemonList: tempPokemon));
   }
